9d1be0a7c33d2425dff2a01d4871c8c623250471
Tycoonlover1359 <tycoonlover1359@protonmail.com>
2020-07-18 20:15:58 -0700

    Moved to /Roblo3; finished CRUD ops for DDB

--- Roblo3/DynamoDB.lua
+++ Roblo3/DynamoDB.lua
@@ -34,6 +34,90 @@ local function serviceResource(accessKeyId, secretAccessKey, region)
         self.ddbTable = {}
         local ddbTable = self.ddbTable
         ddbTable.Name = tableName
+        
+        function ddbTable:DeleteItem(kwargs)
+            if self ~= ddbTable then error("`DeleteItem` must be called with `:`, not `.`", 2) end
+            if type(kwargs) ~= "table" then error("`kwargs` must be a table", 2) end
+            local ddbJson = {}
+            ddbJson["Key"] = {}
+            toDdbJson(kwargs["Key"], ddbJson["Key"])
+            ddbJson["TableName"] = ddbTable.Name
+            ddbJson["ConditionExpression"] = kwargs["ConditionExpression"]
+            ddbJson["ExpressionAttributeNames"] = kwargs["ExpressionAttributeNames"]
+            if kwargs["ExpressionAttributeValues"] ~= nil then
+                ddbJson["ExpressionAttributeValues"] = {}
+                toDdbJson(kwargs["ExpressionAttributeValues"], ddbJson["ExpressionAttributeValues"])
+            end
+            ddbJson["ReturnConsumedCapacity"] = kwargs["ReturnConsumedCapacity"] or "TOTAL"
+            ddbJson["ReturnItemCollectionMetrics"] = kwargs["ReturnItemCollectionMetrics"]
+            ddbJson["ReturnValues"] = kwargs["ReturnValues"]
+
+            local datestamp, amzdate = requestTime()
+
+            local method = "POST"
+            local query = {}
+            local payload = toJson(ddbJson)
+            local path = ""
+            local headers = {
+                ["Host"] = "dynamodb."..ddb.region..".amazonaws.com",
+                ["x-amz-date"] = amzdate,
+                ["x-amz-target"] = "DynamoDB_20120810.DeleteItem",
+                ["Content-Type"] = "application/x-amz-json-1.0"
+            }
+
+            local authItems = {
+                ["method"] = method,
+                ["algorithm"] = ddb.algorithm,
+                ["datestamp"] = datestamp,
+                ["amzdate"] = amzdate,
+                ["region"] = ddb.region,
+                ["service"] = ddb.service,
+                ["secretAccessKey"] = secrets.secretAccessKey,
+                ["accessKeyId"] = secrets.accessKeyId,
+                ["payload"] = payload,
+                ["path"] = path,
+                ["headers"] = headers,
+                ["query"] = query
+            }
+
+            local authHeader, canonicalQueryString = auth.formAuthenticationHeader(authItems)
+
+            headers["Authorization"] = authHeader
+            headers["Host"] = nil
+
+            local url = ddb.endpoint .. path
+            if canonicalQueryString ~= "" then url = url .. "?" .. canonicalQueryString end
+            local requestParams = {
+                ["Url"] = url,
+                ["Method"] = method,
+                ["Headers"] = headers,
+                ["Body"] = payload
+            }
+            local response = request(requestParams)
+            if response.Success then
+                local responseDataRaw = response.Response
+                local body = responseDataRaw.Body
+                local bodyData = toTable(body)
+                local responseData = {}
+                if bodyData.Attributes ~= nil then 
+                    responseData["Attributes"] = {}
+                    fromDdbJson(bodyData.Attributes, responseData["Attributes"])
+                end
+                if bodyData.ItemCollectionMetrics ~= nil then
+                    responseData["ItemCollectionMetrics"] = {}
+                    responseData["ItemCollectionMetrics"]["ItemCollectionKey"] = {}
+                    responseData["ItemCollectionMetrics"]["SizeEstimateRangeGB"] = bodyData["ItemCollectionMetrics"]["SizeEstimateRangeGB"]
+                    fromDdbJson(bodyData["ItemCollectionMetrics"]["ItemCollectionKey"], responseData["ItemCollectionMetrics"]["ItemCollectionKey"])
+                end
+                if bodyData.ConsumedCapacity ~= nil then
+                    responseData["ConsumedCapacity"] = bodyData.ConsumedCapacity
+                end
+                if responseData == {} then responseData = nil end
+                return responseData, bodyData, response
+            else
+                error(response.ErrorType..": "..response.ErrorMessage, 2)
+            end
+        end
 
         function ddbTable:GetItem(kwargs)
             if self ~= ddbTable then error("`GetItem` must be called with `:`, not `.`", 2) end
@@ -169,6 +253,7 @@ local function serviceResource(accessKeyId, secretAccessKey, region)
             ddbJson["ConditionExpression"] = kwargs["ConditionExpression"]
             ddbJson["ExpressionAttributeNames"] = kwargs["ExpressionAttributeNames"]
             if kwargs["ExpressionAttributeValues"] ~= nil then
+                ddbJson["ExpressionAttributeValues"] = {}
                 toDdbJson(kwargs["ExpressionAttributeValues"], ddbJson["ExpressionAttributeValues"])
             end
             ddbJson["ReturnConsumedCapacity"] = kwargs["ReturnConsumedCapacity"] or "TOTAL"
@@ -241,7 +326,92 @@ local function serviceResource(accessKeyId, secretAccessKey, region)
                 error(response.ErrorType..": "..response.ErrorMessage, 2)
             end
         end
-        
+            
+        function ddbTable:UpdateItem(kwargs)
+            if self ~= ddbTable then error("`UpdateItem` must be called with `:`, not `.`", 2) end
+            if type(kwargs) ~= "table" then error("`kwargs` must be a table", 2) end
+            local ddbJson = {}
+            ddbJson["Key"] = {}
+            toDdbJson(kwargs["Key"], ddbJson["Key"])
+            ddbJson["TableName"] = ddbTable.Name
+            ddbJson["ConditionExpression"] = kwargs["ConditionExpression"]
+            ddbJson["ExpressionAttributeNames"] = kwargs["ExpressionAttributeNames"]
+            if kwargs["ExpressionAttributeValues"] ~= nil then
+                ddbJson["ExpressionAttributeValues"] = {}
+                toDdbJson(kwargs["ExpressionAttributeValues"], ddbJson["ExpressionAttributeValues"])
+            end
+            ddbJson["ReturnConsumedCapacity"] = kwargs["ReturnConsumedCapacity"] or "TOTAL"
+            ddbJson["ReturnItemCollectionMetrics"] = kwargs["ReturnItemCollectionMetrics"]
+            ddbJson["ReturnValues"] = kwargs["ReturnValues"]
+            ddbJson["UpdateExpression"] = kwargs["UpdateExpression"]
+
+            local datestamp, amzdate = requestTime()
+
+            local method = "POST"
+            local query = {}
+            local payload = toJson(ddbJson)
+            local path = ""
+            local headers = {
+                ["Host"] = "dynamodb."..ddb.region..".amazonaws.com",
+                ["x-amz-date"] = amzdate,
+                ["x-amz-target"] = "DynamoDB_20120810.UpdateItem",
+                ["Content-Type"] = "application/x-amz-json-1.0"
+            }
+
+            local authItems = {
+                ["method"] = method,
+                ["algorithm"] = ddb.algorithm,
+                ["datestamp"] = datestamp,
+                ["amzdate"] = amzdate,
+                ["region"] = ddb.region,
+                ["service"] = ddb.service,
+                ["secretAccessKey"] = secrets.secretAccessKey,
+                ["accessKeyId"] = secrets.accessKeyId,
+                ["payload"] = payload,
+                ["path"] = path,
+                ["headers"] = headers,
+                ["query"] = query
+            }
+
+            local authHeader, canonicalQueryString = auth.formAuthenticationHeader(authItems)
+
+            headers["Authorization"] = authHeader
+            headers["Host"] = nil
+
+            local url = ddb.endpoint .. path
+            if canonicalQueryString ~= "" then url = url .. "?" .. canonicalQueryString end
+            local requestParams = {
+                ["Url"] = url,
+                ["Method"] = method,
+                ["Headers"] = headers,
+                ["Body"] = payload
+            }
+            local response = request(requestParams)
+            if response.Success then
+                local responseDataRaw = response.Response
+                local body = responseDataRaw.Body
+                local bodyData = toTable(body)
+                local responseData = {}
+                if bodyData.Attributes ~= nil then 
+                    responseData["Attributes"] = {}
+                    fromDdbJson(bodyData.Attributes, responseData["Attributes"])
+                end
+                if bodyData.ItemCollectionMetrics ~= nil then
+                    responseData["ItemCollectionMetrics"] = {}
+                    responseData["ItemCollectionMetrics"]["ItemCollectionKey"] = {}
+                    responseData["ItemCollectionMetrics"]["SizeEstimateRangeGB"] = bodyData["ItemCollectionMetrics"]["SizeEstimateRangeGB"]
+                    fromDdbJson(bodyData["ItemCollectionMetrics"]["ItemCollectionKey"], responseData["ItemCollectionMetrics"]["ItemCollectionKey"])
+                end
+                if bodyData.ConsumedCapacity ~= nil then
+                    responseData["ConsumedCapacity"] = bodyData.ConsumedCapacity
+                end
+                if responseData == {} then responseData = nil end
+                return responseData, bodyData, response
+            else
+                error(response.ErrorType..": "..response.ErrorMessage, 2)
+            end
+        end
+
         return ddbTable
     end
 
