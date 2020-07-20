d7cd34f16a7d70b5a8ef1bbc2d975babafb3880a
Tycoonlover1359 <tycoonlover1359@protonmail.com>
2020-07-19 23:53:18 -0700

    Added `client` initiator

--- Roblo3/init.lua
+++ Roblo3/init.lua
@@ -1,11 +1,11 @@
 local __version = "1.0.0"
 
-local serviceResources = {}
+local services = {}
 
 for _, child in pairs(script:GetDescendants()) do
     if child:IsA("ModuleScript") and not child:IsDescendantOf(script.Utilities) then
         local temp = require(child)
-        serviceResources[string.lower(child.Name)] = temp
+        services[string.lower(child.Name)] = temp
     end
 end
 
@@ -23,7 +23,7 @@ local function resource(resourceName, kwargs)
         secretAccessKey = env["secretAccessKey"]
     end
     if secretAccessKey == nil then error("`secretAccessKey` was not provided", 2) end
-    local service = serviceResources[resourceName]
+    local service = services[resourceName]
     if not service then error("Service `"..resourceName.."` not found") end
     if type(kwargs) ~= "table" then error("kwargs must be a table") end
     local regionName = kwargs["regionName"]
@@ -32,8 +32,27 @@ local function resource(resourceName, kwargs)
     return resource
 end
 
-local function client(resourceName, accessKeyId, secretKeyId, kwargs)
-    error("`client` not currently implemented. Please use `resource` instead.")
+local function client(resourceName, kwargs)
+    local env = getfenv()
+    if kwargs ~= nil and kwargs["accessKeyId"] ~= nil then
+        accessKeyId = kwargs["accessKeyId"]
+    elseif env["accessKeyId"] ~= nil then
+        accessKeyId = env["accessKeyId"]
+    end
+    if accessKeyId == nil then error("`accessKeyId` was not provided", 2) end
+    if kwargs ~= nil and kwargs["secretAccessKey"] ~= nil then
+        secretAccessKey = kwargs["secretAccessKey"]
+    elseif env["secretAccessKey"] ~= nil then
+        secretAccessKey = env["secretAccessKey"]
+    end
+    if secretAccessKey == nil then error("`secretAccessKey` was not provided", 2) end
+    local service = services[resourceName]
+    if not service then error("Service `"..resourceName.."` not found") end
+    if type(kwargs) ~= "table" then error("kwargs must be a table") end
+    local regionName = kwargs["regionName"]
+    if not regionName then regionName = "us-east-1" end
+    local resource = service.client(accessKeyId, secretAccessKey, regionName)
+    return resource
 end
 
 return {resource = resource, client = client}
